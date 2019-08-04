b92763d8185cf944aede4ea167bccc5eb467c994
vrunoa <vruno.alassia@gmail.com>
2019-08-04 13:20:51 -0700

    fix linting

--- tests/test_animations.js
+++ tests/test_animations.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer-core');
 const DEFAULT_PLAYBACK_RATE = 2;
 let PLAYBACK_RATE = DEFAULT_PLAYBACK_RATE;
 if (process.env.PLAYBACK_RATE && !isNaN(process.env.PLAYBACK_RATE)) {
-  PLAYBACK_RATE = parseInt(process.env.PLAYBACK_RATE)
+  PLAYBACK_RATE = parseInt(process.env.PLAYBACK_RATE);
 }
 
 let driver;
@@ -16,30 +16,30 @@ let caps = {
   chromeOptions: {
     args: ['--remote-debugging-port=9222']
   }
-}
+};
 
-describe('simple selenium chrome test', async function() {
+describe('simple selenium chrome test', async () => {
   before(async () => {
     driver = await wd.promiseChainRemote('http://localhost:4444/wd/hub');
-    res = await driver.init(caps)
+    let res = await driver.init(caps);
     console.log(`Session ID ${res[0]}`);
   });
   after(async () => {
-    await driver.quit()
+    await driver.quit();
   });
-  it('should run a test in chrome', async function() {
-    let browser = await puppeteer.connect({browserURL:"http://127.0.0.1:9222"});
+  it('should run a test in chrome', async () => {
+    let browser = await puppeteer.connect({browserURL:'http://127.0.0.1:9222'});
     let pages = await browser.pages();
     for (let i=0;i<pages.length;i++) {
       let page = pages[0];
       const client = await page.target().createCDPSession();
       await client.send('Animation.enable');
-      console.log(`Setting Animation playbackRate to :${PLAYBACK_RATE}`)
+      console.log(`Setting Animation playbackRate to :${PLAYBACK_RATE}`);
       await client.send('Animation.setPlaybackRate', {
         playbackRate: PLAYBACK_RATE
       });
     }
-    await driver.get("https://www.w3schools.com/css/css3_animations.asp");
+    await driver.get('https://www.w3schools.com/css/css3_animations.asp');
     let el = await driver.elementById('animated_div');
     expect(el).to.not.equal(null);
   });
